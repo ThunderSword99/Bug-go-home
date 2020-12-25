6800da333d50e56e8189547fd67e57691502fe11
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-12-25 15:53:37 +0700

    create 999 stages

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public GameObject levelPrefab;
+    public int numberOfLevels = 999;
+
+    void LoadLevel()
+    {
+        for (int i=2;i<=numberOfLevels;i++)
+        {
+            GameObject go = Instantiate(levelPrefab, transform.position, Quaternion.identity);
+            go.GetComponent<Stage>().SetLevel(i);
+            go.transform.SetParent(levelHolder.transform,false);
+                
+            
+        }
+        
+    }
+
+    void Start()
+    {
+        LoadLevel();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage : MonoBehaviour
+{
+    private int currentLevel;
+    private int star=0;
+    private bool isLock=true;
+
+    public GameObject TextGo;
+    public GameObject LockGo;
+    public GameObject Star1;
+    public GameObject Star2;
+    public GameObject Star3;
+
+    void Start()
+    {
+       TextGo.GetComponent<Text>().text = currentLevel.ToString();
+       if (isLock)
+       {
+            LockGo.active = true;
+            Star1.active = false;
+            Star2.active = false;
+            Star3.active = false;
+       }
+    }
+
+    public void SetLevel(int i)
+    {
+        currentLevel = i;
+    }
+}
