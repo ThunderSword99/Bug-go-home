dfb2577f726bc186a5b5e820894e65c3844b0f72
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-12-27 13:39:19 +0700

    create columnLine and rowLine UI

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject rowLineHolder;
+    public GameObject columnLineLeftHolder;
+    public GameObject columnLineRightHolder;
     public GameObject levelHolder;
     public GameObject levelPrefab;
+    public GameObject rowLine;
+    public GameObject columnLine;
     public int numberOfLevels = 999;
+
+    private List<GameObject> StageList = new List<GameObject>();
 
     void LoadLevel()
     {
-        for (int i=2;i<=numberOfLevels;i++)
+        for (int i=1;i<=numberOfLevels;i++)
         {
             GameObject go = Instantiate(levelPrefab, transform.position, Quaternion.identity);
             go.GetComponent<Stage>().SetLevel(i);
             go.transform.SetParent(levelHolder.transform,false);
-                
+            StageList.Add(go);
+            if (i%4==1)
+            {
+                GameObject newRowLine = Instantiate(rowLine, transform.position, Quaternion.identity);
+                newRowLine.transform.SetParent(rowLineHolder.transform,false);
+            }
+            if (i%8==0)
+            {
+                GameObject newColumnLeftLine = Instantiate(columnLine, transform.position, Quaternion.identity);
+                newColumnLeftLine.transform.SetParent(columnLineLeftHolder.transform,false);
+
+                GameObject newColumnRightLine = Instantiate(columnLine, transform.position, Quaternion.identity);
+                newColumnRightLine.transform.SetParent(columnLineRightHolder.transform,false);
+            }
             
         }
         
