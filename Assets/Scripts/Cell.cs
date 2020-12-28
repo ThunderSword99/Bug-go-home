7054ababa07a6805a62e7cb1a91eff9fb3d49c4c
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-12-28 10:35:19 +0700

    demo level1

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool isVisited;
+
+    public GameObject leftWall;
+    public GameObject downWall;
+    public GameObject rightWall;
+    public GameObject upWall;
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject floor;
+    public int rows = 13;
+    public int columns = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0;i< rows;i++)
+        {
+            for (int j=0;j<columns;j++)
+            {
+                Instantiate(floor,new Vector3((j * Screen.width)/rows, 0, (i* Screen.height)/columns), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
