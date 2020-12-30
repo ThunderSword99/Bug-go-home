fe6c3d632cb7a74a64a5c019fe28c935b5d9ae75
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-12-30 23:21:14 +0700

    generate map successed

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,10 +5,45 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     public bool isVisited;
+    public bool isNeighborVisited;
     public Vector3 pos;
 
-    public GameObject leftWall;
-    public GameObject downWall;
-    public GameObject rightWall;
-    public GameObject upWall;
+    public List<GameObject> listWall;
+    public GameObject leftWall;// 1
+    public GameObject downWall;// 2
+    public GameObject rightWall;// 3
+    public GameObject upWall;// 4
+
+    public int getWallNumb()
+    {
+        return listWall.Count;
+    }
+
+    public void DestroyWall(int i)
+    {
+        
+        if (i==1)
+        {
+            listWall.Remove(leftWall);
+            Destroy(leftWall);
+        }
+        else if (i==2)
+        {
+            listWall.Remove(downWall);
+            Destroy(downWall);
+        }
+        else if (i==3)
+        {
+            listWall.Remove(rightWall);
+            Destroy(rightWall);
+        }
+        else if (i==4)
+        {
+            listWall.Remove(upWall);
+            Destroy(upWall);
+        }
+        
+    }
+
+
 }
--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -13,7 +13,11 @@ public class Maze : MonoBehaviour
     public int columns = 10;
     
     public GameObject[,] listCell;
+
+    int k=0;
    
+    //private int[4] _X = {};
+    //private int[4] _Y = {};
 
     // Start is called before the first frame update
     void Start()
@@ -30,9 +34,135 @@ public class Maze : MonoBehaviour
             }
         }
 
-
-        BFS();
+        //DestroyRandomWall(listCell[3,3]);
+        //BFS();
+        GenerateMap();
+        //int[] x = new int [4];
+        //x = suffefArray();
+        //Debug.Log(x[0] + " " + x[1] + " " + x[2] + " " + x[3]);
     }
+    
+    private void DestroyNeighborWall(Cell currentCell,int way)
+    {
+        try
+        {
+            if (way == 1) // left
+            {
+                Cell neighborCell = listCell[(int)currentCell.pos.x,(int)currentCell.pos.y-1].GetComponent<Cell>();
+                neighborCell.DestroyWall(3);
+            }
+            else if (way == 2) // down
+            {
+                Cell neighborCell = listCell[(int)currentCell.pos.x+1,(int)currentCell.pos.y].GetComponent<Cell>();
+                neighborCell.DestroyWall(4);
+            }
+            else if (way == 3) // right
+            {
+                Cell neighborCell = listCell[(int)currentCell.pos.x,(int)currentCell.pos.y+1].GetComponent<Cell>();
+                neighborCell.DestroyWall(1);
+            }
+            else if (way == 4) // up
+            {
+                Cell neighborCell = listCell[(int)currentCell.pos.x-1,(int)currentCell.pos.y].GetComponent<Cell>();
+                neighborCell.DestroyWall(2);
+            }
+        }
+        catch
+        {
+        
+        }
+        
+    }
+
+    private void DestroyWall(Cell cell,int way)
+    {
+        cell.DestroyWall(way);
+        DestroyNeighborWall(cell,way);
+    }
+
+    private void GenerateMap()
+    {
+        Cell firstCell = listCell[0,0].GetComponent<Cell>();
+        DFS(firstCell);
+    }
+    
+    private Cell getNextCell(Cell currentCell,int way)
+    {
+        try
+        {
+            if (way == 1) // left
+            {
+                return listCell[(int)currentCell.pos.x,(int)currentCell.pos.y-1].GetComponent<Cell>();
+            }
+            else if (way == 2) // down
+            {
+                return listCell[(int)currentCell.pos.x+1,(int)currentCell.pos.y].GetComponent<Cell>();
+            }
+            else if (way == 3) // right
+            {
+                return listCell[(int)currentCell.pos.x,(int)currentCell.pos.y+1].GetComponent<Cell>();
+            }
+            else if (way == 4) // up
+            {
+                return listCell[(int)currentCell.pos.x-1,(int)currentCell.pos.y].GetComponent<Cell>();
+            }
+        }
+        catch
+        {
+            return null;
+        }
+        return null;
+    }
+
+    private int[] suffefArray()
+    {
+        int[] temp = new int[4];
+        temp[0] = UnityEngine.Random.Range(1,5);
+        do
+        {
+            temp[1] = UnityEngine.Random.Range(1,5);
+        }while(temp[1]==temp[0]);   
+        do
+        {
+            temp[2] = UnityEngine.Random.Range(1,5);
+        }while(temp[2]==temp[1] || temp[2]==temp[0]);
+        temp[3] = 10 - temp[0] - temp[1] - temp[2];
+        return temp;
+    }
+
+    private void DFS(Cell cell)
+    {
+        cell.GetComponent<Image>().color = Color.red;
+        cell.isVisited = true;
+
+        int[] randomWay = suffefArray();
+        
+        k++;
+        if (k>=130)
+        {
+            return;
+        }
+
+        for (int i=0;i<4;i++)
+        {
+            Cell nextCell = getNextCell(cell,randomWay[i]);
+            
+            if (nextCell==null)
+            {
+                continue;
+            }
+            
+            if (!nextCell.isVisited)
+            {
+                DestroyWall(cell,randomWay[i]);
+                DFS(nextCell);
+            }
+            
+            
+        }
+
+    }
+
 
     private void BFS()
     {
@@ -68,14 +198,14 @@ public class Maze : MonoBehaviour
                     if (!tempCell.isVisited)
                     {
                         tempCell.pos.z = pos.z;
-                        tempGo.GetComponent<Image>().color = Color.red;
+                        //tempGo.GetComponent<Image>().color = Color.red;
                         tempCell.isVisited = true;
                         q.Enqueue(tempGo);
                     }
                 }
                 catch
                 {
-                     Debug.Log("Horrible things happened!");
+                     //Debug.Log("Horrible things happened!");
                 }
             }
         }
